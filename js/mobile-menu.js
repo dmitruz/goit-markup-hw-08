7f60223e3cbb4499f60ad073c4ae088dac6985ac
dmitruz <dmitruz970@gmail.com>
2021-08-02 23:26:48 +0300

    mlm;l

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,29 +1,43 @@
-(() => {
-    const mobileMenu = document.querySelector('.js-menu-container');
-    const openMenuBtn = document.querySelector('.js-open-menu');
-    const closeMenuBtn = document.querySelector('.js-close-menu');
-  
-    const toggleMenu = () => {
-      const isMenuOpen =
-        openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-      mobileMenu.classList.toggle('is-open');
-  
-      const scrollLockMethod = !isMenuOpen
-        ? 'disableBodyScroll'
-        : 'enableBodyScroll';
-      bodyScrollLock[scrollLockMethod](document.body);
-    };
-  
-    openMenuBtn.addEventListener('click', toggleMenu);
-    closeMenuBtn.addEventListener('click', toggleMenu);
-  
-    // Закрываем мобильное меню на более широких экранах
-    // в случае изменения ориентации устройства.
-    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-      if (!e.matches) return;
-      mobileMenu.classList.remove('is-open');
-      openMenuBtn.setAttribute('aria-expanded', false);
-      bodyScrollLock.enableBodyScroll(document.body);
-    });
-  })();
+
+
+  const focusableElements =
+  "button, a[href], input, select, textarea, [tabindex]:not([tabindex='-1'])";
+const modalWindow = document.querySelector("[data-modal]"); // select the modal by it's attr
+
+// get first element to be focused inside modal
+const firstFocusableElement = modalWindow.querySelectorAll(focusableElements)[0];
+
+const focusableContent = modalWindow.querySelectorAll(focusableElements);
+
+// get last element to be focused inside modal
+const lastFocusableElement = focusableContent[focusableContent.length - 1];
+
+modalWindow.addEventListener("keydown", function (e) {
+  if (modalWindow.classList.contains("is-hidden")) {
+    return;
+  } else {
+    let isTabPressed = e.key === "Tab" || e.keyCode === 9;
+
+    if (!isTabPressed) {
+      return;
+    }
+
+    if (e.shiftKey) {
+      // if shift key pressed for shift + tab combination
+      if (
+        document.activeElement === firstFocusableElement ||
+        document.activeElement === modalWindow
+      ) {
+        lastFocusableElement.focus(); // add focus for the last focusable element
+        e.preventDefault();
+      }
+    } else {
+      // if tab key is pressed
+      if (document.activeElement === lastFocusableElement) {
+        // if focused has reached to last focusable element then focus first focusable element after pressing tab
+        firstFocusableElement.focus(); // add focus for the first focusable element
+        e.preventDefault();
+      }
+    }
+  }
+});
